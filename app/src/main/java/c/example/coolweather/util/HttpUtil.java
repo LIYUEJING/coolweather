c62c739869fdedab71b9d551f70c7e1884bbfe76
LYJ <767692169@qq.com>
2019-04-09 22:00:39 +0800

    完成遍历省市县三级列表的功能。

---
+++ app/src/main/java/c/example/coolweather/util/HttpUtil.java
@@ -0,0 +1,17 @@
+package c.example.coolweather.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by 76769 on 2019/4/8.
+ */
+
+public class HttpUtil {
+
+    public static void sendOkHttpRequest(String address, okhttp3.Callback callback) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+}
